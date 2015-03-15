924e466113f0b493f8082b90f54356702587b8f8
themadhukiran <pmadhukiran96@gmail.com>
2015-03-15 07:42:23 +0530

    cleaned ClipboardService

--- app/src/main/java/in/workarounds/define/service/ClipboardService.java
+++ app/src/main/java/in/workarounds/define/service/ClipboardService.java
@@ -5,69 +5,69 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
 import android.os.IBinder;
-import android.util.Log;
 import android.webkit.URLUtil;
 
-import in.workarounds.define.BuildConfig;
 import in.workarounds.define.util.LogUtils;
 
 public class ClipboardService extends Service implements
-		ClipboardManager.OnPrimaryClipChangedListener {
+        ClipboardManager.OnPrimaryClipChangedListener {
     private static final String TAG = LogUtils.makeLogTag(ClipboardService.class);
     private static boolean isRunning = false;
 
-	@Override
-	public void onCreate() {
-		isRunning = true;
-		ClipboardManager clipboardManager = getClipboardManager();
-		clipboardManager.addPrimaryClipChangedListener(this);
-	}
+    @Override
+    public void onCreate() {
+        isRunning = true;
+        ClipboardManager clipboardManager = getClipboardManager();
+        clipboardManager.addPrimaryClipChangedListener(this);
+    }
 
-	private ClipboardManager getClipboardManager() {
-		return (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-	}
+    private ClipboardManager getClipboardManager() {
+        return (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+    }
 
-	@Override
-	public void onDestroy() {
-		isRunning = false;
-	}
+    @Override
+    public void onDestroy() {
+        isRunning = false;
+    }
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		return START_STICKY;
-	}
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
 
-	public static boolean isRunning() {
-		return isRunning;
-	}
+    public static boolean isRunning() {
+        return isRunning;
+    }
 
-	@Override
-	public void onPrimaryClipChanged() {
-		if(!PopupManager.isPopupShown()) showPopupIfClipIsText();
-	}
+    @Override
+    public void onPrimaryClipChanged() {
+        if (!PopupManager.isPopupShown()) {
+            String text = getClipData();
+            startActionResolver(text);
+        }
+    }
 
-	private void showPopupIfClipIsText() {		
-		ClipData clipData = getClipboardManager().getPrimaryClip();
-		ClipData.Item item = clipData.getItemAt(0);
-		CharSequence text = item.getText();
-		boolean isUrl = false;
-		if(text != null) URLUtil.isValidUrl(text.toString());
-		if (text != null && !isUrl) {
-			Intent intent = new Intent(this, PopupManager.class);
-			intent.putExtra(PopupManager.INTENT_EXTRA_CLIPTEXT, text.toString());
-			getBaseContext().startService(intent);
-		}
-	}
+    private String getClipData() {
+        ClipData clipData = getClipboardManager().getPrimaryClip();
+        ClipData.Item item = clipData.getItemAt(0);
+        CharSequence text = item.getText();
+        if(text != null && !URLUtil.isValidUrl(text.toString())) {
+            return text.toString();
+        } else {
+            return null;
+        }
+    }
 
-	public static void printLog(String text) {
-		boolean debug = BuildConfig.DEBUG;
-		if (debug)
-			Log.d(TAG, text);
-	}
-
+    private void startActionResolver(String text) {
+        if (text != null) {
+            Intent intent = new Intent(this, PopupManager.class);
+            intent.putExtra(PopupManager.INTENT_EXTRA_CLIPTEXT, text);
+            getBaseContext().startService(intent);
+        }
+    }
 }
