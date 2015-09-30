b1a699daabb90e64e24194e930962a84a16d4da5
themadhukiran <pmadhukiran96@gmail.com>
2015-09-30 23:47:43 +0530

    add loading indicators

--- app/src/main/java/in/workarounds/define/view/meaning/MeaningPresenter.java
+++ app/src/main/java/in/workarounds/define/view/meaning/MeaningPresenter.java
@@ -1,17 +1,24 @@
 package in.workarounds.define.view.meaning;
 
 import android.os.AsyncTask;
+import android.support.annotation.IntDef;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import in.workarounds.define.R;
 import in.workarounds.define.dictionary.Dictionary;
 import in.workarounds.define.dictionary.DictionaryException;
 import in.workarounds.define.dictionary.Result;
 import in.workarounds.define.portal.PerPortal;
 import in.workarounds.define.util.LogUtils;
+import in.workarounds.typography.TextView;
 
 /**
  * Created by madki on 26/09/15.
@@ -19,11 +26,17 @@ import in.workarounds.define.util.LogUtils;
 @PerPortal
 public class MeaningPresenter {
     private static final String TAG = LogUtils.makeLogTag(MeaningPresenter.class);
+    private static final int LOAD_STATUS   = 1;
+    private static final int LOAD_PROGRESS = 2;
+    private static final int MEANING_LIST  = 3;
 
     private Dictionary dictionary;
     private MeaningPage meaningPage;
     private String word;
     private MeaningsAdapter adapter = new MeaningsAdapter();
+    private TextView loadStatus;
+    private ProgressBar loadProgress;
+    private RecyclerView meaningList;
 
     @Inject
     public MeaningPresenter(Dictionary dictionary) {
@@ -32,15 +45,18 @@ public class MeaningPresenter {
 
     public void addView(MeaningPage view) {
         this.meaningPage = view;
+        initViews();
+        setInitialViews();
     }
 
     public void dropView() {
         this.meaningPage = null;
+        dropViews();
     }
 
     public void word(String word) {
         if(word != null && !word.equals(this.word)) {
-            adapter.update(new ArrayList<Result>());
+            showProgress();
         }
         this.word = word;
         if(meaningPage != null) {
@@ -59,6 +75,11 @@ public class MeaningPresenter {
 
     private void onResultsUpdated(List<Result> results) {
         adapter.update(results);
+        if(results != null && results.size() != 0) {
+            showStatus("Sorry, no results found.");
+        } else {
+            showList();
+        }
     }
 
     private class MeaningsTask extends AsyncTask<String, Integer, List<Result>> {
@@ -78,5 +99,64 @@ public class MeaningPresenter {
         protected void onPostExecute(List<Result> results) {
             onResultsUpdated(results);
         }
+    }
+
+    private void initViews() {
+        loadStatus = (TextView) meaningPage.findViewById(R.id.tv_load_status);
+        loadProgress = (ProgressBar) meaningPage.findViewById(R.id.pb_load_progress);
+        meaningList = (RecyclerView) meaningPage.findViewById(R.id.rv_meaning_list);
+    }
+
+    private void setInitialViews() {
+        if(TextUtils.isEmpty(word)) {
+            showStatus("Please select a word to define. Tap on a word to select one. Or swipe to select multiple words.");
+        } else if(adapter != null && adapter.getItemCount() != 0) {
+            showList();
+        } else {
+            showStatus("Sorry, no results found");
+        }
+    }
+
+    private void dropViews() {
+        loadStatus = null;
+        loadProgress = null;
+        meaningList = null;
+    }
+
+    private void showStatus(String status) {
+        loadStatus.setText(status);
+        showView(LOAD_STATUS);
+    }
+
+    private void showProgress() {
+        showView(LOAD_PROGRESS);
+    }
+
+    private void showList() {
+        showView(MEANING_LIST);
+    }
+
+    private void showView(@ViewEnum int view) {
+        switch (view) {
+            case LOAD_STATUS:
+                changeViewVisibilities(true, false, false);
+                break;
+            case LOAD_PROGRESS:
+                changeViewVisibilities(false, true, false);
+                break;
+            case MEANING_LIST:
+                changeViewVisibilities(false, false, true);
+                break;
+        }
+    }
+
+    private void changeViewVisibilities(boolean status, boolean progress, boolean list) {
+        loadStatus.setVisibility(status?View.VISIBLE:View.GONE);
+        loadProgress.setVisibility(progress?View.VISIBLE:View.GONE);
+        meaningList.setVisibility(list?View.VISIBLE:View.GONE);
+    }
+
+    @IntDef({LOAD_PROGRESS, LOAD_STATUS, MEANING_LIST})
+    private @interface ViewEnum {
     }
 }
