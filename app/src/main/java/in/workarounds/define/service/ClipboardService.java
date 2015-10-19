d3e23e125dead69920fc1117049ad655d4bc3156
NithinNara <nithin.nara@outlook.com>
2015-10-19 17:24:38 +0530

    Add notifications for silent and priority modes

--- app/src/main/java/in/workarounds/define/service/ClipboardService.java
+++ app/src/main/java/in/workarounds/define/service/ClipboardService.java
@@ -1,21 +1,32 @@
 package in.workarounds.define.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.webkit.URLUtil;
 
+import in.workarounds.define.R;
 import in.workarounds.define.portal.MainPortal;
+import in.workarounds.define.ui.activity.UserPrefActivity;
 import in.workarounds.define.util.LogUtils;
+import in.workarounds.define.util.PrefUtils;
 import in.workarounds.portal.Portal;
 
 public class ClipboardService extends Service implements
         ClipboardManager.OnPrimaryClipChangedListener {
     private static final String TAG = LogUtils.makeLogTag(ClipboardService.class);
     private static boolean isRunning = false;
+    private static final String INTENT_SILENT_NOTIFICATION_CLICK_SELECTED_TEXT_KEY
+            = "INTENT_SILENT_NOTIFICATION_CLICK_SELECTED_TEXT_KEY";
+    private static final int SILENT_NOTIFICATION_ID = 201;
 
     @Override
     public void onCreate() {
@@ -35,6 +46,12 @@ public class ClipboardService extends Service implements
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        if(intent.hasExtra(INTENT_SILENT_NOTIFICATION_CLICK_SELECTED_TEXT_KEY)){
+            startPortal(intent.getStringExtra(INTENT_SILENT_NOTIFICATION_CLICK_SELECTED_TEXT_KEY));
+            //remove notification
+            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.cancel(SILENT_NOTIFICATION_ID);
+        }
         return START_STICKY;
     }
 
@@ -65,6 +82,41 @@ public class ClipboardService extends Service implements
     }
 
     private void startActionResolver(String text) {
+        @UserPrefActivity.NotifyMode int notifyMode = PrefUtils.getNotifyMode(UserPrefActivity.OPTION_DIRECT,this);
+        if(notifyMode == UserPrefActivity.OPTION_SILENT || notifyMode == UserPrefActivity.OPTION_PRIORITY) {
+//Set notification priority as high for priority mode, default for silent mode
+            int priority =  (notifyMode == UserPrefActivity.OPTION_PRIORITY)
+                    ? NotificationCompat.PRIORITY_HIGH : NotificationCompat.PRIORITY_DEFAULT;
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.ic_notification_icon)
+                            .setPriority(priority)
+                            .setVibrate(new long[0]) //mandatory for high priority,setting no vibration
+                            .setContentTitle(getString(R.string.app_name))
+                            .setContentText(getString(R.string.notification_content));
+// Creates an explicit intent for clipboard Service
+            Intent resultIntent = new Intent(this, ClipboardService.class);
+            resultIntent.putExtra(INTENT_SILENT_NOTIFICATION_CLICK_SELECTED_TEXT_KEY, text);
+            PendingIntent resultPendingIntent =
+                    PendingIntent.getService(
+                            this,
+                            0,
+                            resultIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// SILENT_NOTIFICATION_ID allows you to update/remove the notification later on.
+            mNotificationManager.notify(SILENT_NOTIFICATION_ID, mBuilder.build());
+        }
+        else{
+            startPortal(text);
+        }
+    }
+
+    private void startPortal(String text){
         // TODO check if portal already open. If yes send data and don't re-open
         Bundle bundle = new Bundle();
         bundle.putString(MainPortal.BUNDLE_KEY_CLIP_TEXT, text);
