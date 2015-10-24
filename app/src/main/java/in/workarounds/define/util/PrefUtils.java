2a57ec133df8eedbface967a4c603df56715c635
manidesto <manideepiitb@gmail.com>
2015-10-25 00:32:47 +0530

    Adds pref util functions to store user state
    Stores whether the user has finished tutorial.
    Stores whether dictionary is unzipped or not.
    Stores whether the user has downloaded an offline dictionary

--- app/src/main/java/in/workarounds/define/util/PrefUtils.java
+++ app/src/main/java/in/workarounds/define/util/PrefUtils.java
@@ -2,7 +2,9 @@ package in.workarounds.define.util;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 
+import in.workarounds.define.api.Constants;
 import in.workarounds.define.ui.activity.UserPrefActivity;
 
 /**
@@ -11,6 +13,10 @@ import in.workarounds.define.ui.activity.UserPrefActivity;
 public class PrefUtils {
     private static final String FILE_NAME = "default.prefs";
     public static final String KEY_NOTIFY_MODE = "key_notify_mode";
+    public static final String KEY_TUTORIAL_DONE = "key_tutorial_done";
+    public static final String KEY_DICTIONARIES_DONE = "key_dictionaries_done";
+
+    private static final String KEY_WORDNET_UNZIPPED = "key_wordnet_unzipped";
 
     private static final String DM = "dm_";
 
@@ -48,5 +54,51 @@ public class PrefUtils {
         SharedPreferences.Editor editor = preferences.edit();
         editor.remove(DM + key);
         editor.apply();
+    }
+
+    public static void setUnzipped(String key, boolean unzipped, Context context){
+        String prefKey = getPrefKeyUnzipped(key);
+        if(prefKey != null){
+            SharedPreferences prefs = getSharedPreferences(context);
+            prefs.edit().putBoolean(prefKey, unzipped).apply();
+        }
+    }
+
+    public static boolean getUnzipped(String key, Context context) {
+        String prefKey = getPrefKeyUnzipped(key);
+        return prefKey != null && getSharedPreferences(context).getBoolean(prefKey, false);
+    }
+
+    public static void setTutorialDone(boolean done, Context context){
+        getSharedPreferences(context)
+                .edit()
+                .putBoolean(KEY_TUTORIAL_DONE, done)
+                .apply();
+    }
+
+    public static boolean getTutorialDone(Context context){
+        return getSharedPreferences(context).getBoolean(KEY_TUTORIAL_DONE, false);
+    }
+
+    public static void setDictionariesDone(boolean done, Context context){
+        getSharedPreferences(context)
+                .edit()
+                .putBoolean(KEY_DICTIONARIES_DONE, done)
+                .apply();
+    }
+
+    public static boolean getDictionariesDone(Context context){
+        return getSharedPreferences(context).getBoolean(KEY_DICTIONARIES_DONE, false);
+    }
+
+    @Nullable
+    private static String getPrefKeyUnzipped(String dictionaryKey){
+        if(dictionaryKey == null){
+            return null;
+        } else if(dictionaryKey.equals(Constants.WORDNET)){
+            return KEY_WORDNET_UNZIPPED;
+        } else {
+            return null;
+        }
     }
 }
