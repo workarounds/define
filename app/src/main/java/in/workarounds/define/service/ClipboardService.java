654dc71aed87ce1088bf7a67e165c2dd04678ea0
manidesto <manideepiitb@gmail.com>
2015-10-25 00:37:37 +0530

    Fixes ClipboardService NPE when intent is null

--- app/src/main/java/in/workarounds/define/service/ClipboardService.java
+++ app/src/main/java/in/workarounds/define/service/ClipboardService.java
@@ -1,6 +1,5 @@
 package in.workarounds.define.service;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -46,7 +45,7 @@ public class ClipboardService extends Service implements
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(intent.hasExtra(INTENT_SILENT_NOTIFICATION_CLICK_SELECTED_TEXT_KEY)){
+        if(intent != null && intent.hasExtra(INTENT_SILENT_NOTIFICATION_CLICK_SELECTED_TEXT_KEY)){
             startPortal(intent.getStringExtra(INTENT_SILENT_NOTIFICATION_CLICK_SELECTED_TEXT_KEY));
             //remove notification
             NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
