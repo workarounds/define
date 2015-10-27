9cbe6d3df43a1e1a95b5cc07c4f288dd66605b05
NithinNara <nithin.nara@outlook.com>
2015-10-27 15:30:28 +0530

    Add portriat screenlock to windowManager params

--- app/src/main/java/in/workarounds/define/portal/MainPortal.java
+++ app/src/main/java/in/workarounds/define/portal/MainPortal.java
@@ -5,9 +5,12 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -68,6 +71,14 @@ public class MainPortal extends Portal implements ComponentProvider {
     @Override
     public PortalComponent component() {
         return component;
+    }
+
+    @NonNull
+    @Override
+    protected WindowManager.LayoutParams getLayoutParams() {
+        WindowManager.LayoutParams params = super.getLayoutParams();
+        params.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        return params;
     }
 
     private void initViews() {
